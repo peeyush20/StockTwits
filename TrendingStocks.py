8b22122ff1e6cb0311ca5e16d7869ec51b754c7c
Peeyush Jain <jainpeeyush20@gmail.com>
2018-09-15 01:31:07 -0400

    Contains api call to get list of 30 trending stocks

---
+++ TrendingStocks.py
@@ -0,0 +1,32 @@
+import requests
+import json
+# Credentials you get from registering a new application
+client_id = 'dc52798ba75f57f7'
+
+data = {
+  'client_id': client_id,
+  'response_type': 'code',
+  'redirect_uri': 'http://www.example.com',
+  'scope': 'read,watch_lists,publish_messages,publish_watch_lists,follow_users,follow_stocks',
+  'prompt': 1
+}
+
+# Retrieve list of 30 trending equities
+r = requests.get('https://api.stocktwits.com/api/2/trending/symbols/equities.json')
+ans = json.loads(r.text)
+
+for i in range(30):
+    print(ans['symbols'][i]['symbol'])
+
+# Print message about specific stock:
+# Stock at index 0
+query = 'https://api.stocktwits.com/api/2/streams/symbol/' + ans['symbols'][0]['symbol'] + '.json'
+# print(query)
+r = ''
+r = requests.get(query)
+
+ans = ''
+ans = json.loads(r.text)
+
+for i in range(30):
+    print(ans['messages'][i]['body'])
